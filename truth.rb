750bec722a2d10365bebaf7d126b61c86bb24a25
bricktsre <gwoodward21@gmail.com>
2019-03-31 21:09:42 -0400

    Finished all the code!

--- truth.rb
+++ truth.rb
@@ -12,13 +12,39 @@ def check_args(t, f, size)
   true
 end
 
+def and_op(row)
+  temp = true
+  row.each do |y|
+    temp = temp && y
+  end
+  temp
+end
+
+def or_op(row)
+  temp = false
+  row.each do |y|
+    temp = temp || y
+  end
+  temp
+end
+
+def operations(data, size)
+  data.each_index do |x|
+    data[x][size] = and_op(data[x])
+    data[x][size+1] = or_op(data[x])
+    data[x][size+2] = !and_op(data[x])
+    data[x][size+3] = !or_op(data[x])
+  end
+  data
+end
+
 def data(size)
-  data = Array.new((2 ** size),0){Array.new(size,0)}
+  data = Array.new((2 ** size), false){Array.new(size, false)}
   data.each_index do |x|
     temp = x
     data[0].each_index do |y|
       if temp - (2 ** ((size-1)-y)) >= 0
-        data[x][y] = 1
+        data[x][y] = true
 	temp -= (2 ** ((size-1)-y))
       end
     end
@@ -28,12 +54,13 @@ end
 
 def generate_table(t, f, size)
   data=data(size)
-  (0...data.size).each do |x|
-    (0...data[0].size).each do |y|
-      print (data[x][y] == 1 ? "#{t}" : "#{f}")
+  operations = operations(data, size) 
+  operations.each_index do |x|
+    operations[x].each_index do |y|
+      operations[x][y] = ( operations[x][y] ? "#{t}" : "#{f}")
     end
-    puts
   end
+  operations
 end
 
 get '/' do
@@ -51,13 +78,14 @@ get '/table' do
   t_symbol, f_symbol, size = params['true'], params['false'], params['size'].to_i
   t_symbol = "T" if t_symbol == ""
   f_symbol = "F" if f_symbol == ""
-  size = 3 if size == ""
+  size = 3 if size == 0
   puts "true:#{t_symbol} false:#{f_symbol} size:#{size}"
   unless check_args(t_symbol, f_symbol, size)
     puts 'invalid parameters'
     erb :invalid_parameters_error
   else
     puts 'Arguments are all good'
-    generate_table(t_symbol, f_symbol, size)
+    table = generate_table(t_symbol, f_symbol, size)
+    erb :output, :locals => {size: size, chart: table}
   end
 end
